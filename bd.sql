25e6f303edcdf36f189e23fa0759ad6316829929
adriancg98 <adri10cg@gmail.com>
2021-05-19 17:55:03 +0200

    BBDD

--- bd.sql
+++ bd.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS 'usuarios' (
   'id' int(10) NOT NULL,
   'nombre_completo' varchar(50) NOT NULL,
   'correo' varchar(75) NOT NULL,
-  'usuario' int(25) NOT NULL,
+  'usuario' varchar(25) NOT NULL,
   'contrasena' varchar(255) NOT NULL,
   'rol' varchar(20) NOT NULL,
   PRIMARY KEY ('id'),
