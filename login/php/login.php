c49affdc4de2ebee7a3ecca6feabf477c472b422
adriancg98 <adri10cg@gmail.com>
2021-05-19 17:54:02 +0200

    BBDD

--- login/php/login.php
+++ login/php/login.php
@@ -1,20 +1,23 @@
 <?php
 include 'conexion_be.php';
 
+
 $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
-$db = getDB();
-$hash_password = hash('sha256', $contrasena); //Password encryption 
-$stmt = $db->prepare("SELECT id FROM usuarios WHERE usuario=:$usuario AND contrasena=:$hash_password");
-$stmt->bindParam("usuario", $usuario, PDO::PARAM_STR);
-$stmt->bindParam("hash_password", $hash_password, PDO::PARAM_STR);
-$stmt->execute();
-$count = $stmt->rowCount();
-$data = $stmt->fetch(PDO::FETCH_OBJ);
-$db = null;
-if ($count) {
-    $_SESSION['id'] = $data->id;
-    return true;
+$contrasena = hash('sha512', $contrasena);
+
+$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '$usuario'");
+
+if (mysqli_num_rows($validar_login) > 0 && password_verify($_POST['contrasena'], $usuario->password)) {
+    $_SESSION['usuario'] = $usuario;
+    header("location: ../../index.php");
+    exit();
 } else {
-    return false;
+    echo '
+            <script>
+                alert("Usuario o contraseña incorrecta. Verifique los datos");
+                window.location = "../index.php";
+            </script>
+        ';
+    exit();
 }
