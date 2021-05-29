bce0c5d10f63585d88b7e6d6a58c36604bbfa9ee
adriancg98 <adri10cg@gmail.com>
2021-05-29 10:49:14 +0200

    cambio

--- citas/inc/usarBD.php
+++ citas/inc/usarBD.php
@@ -1,3 +1,3 @@
 <?php
-  $conexion = mysqli_connect("den1.mysql3.gear.host", "mariobarber", "Ik6x_B8-5xfT", "mariobarber");
+  $conexion = mysqli_connect("den1.mysql1.gear.host", "mariobarber", "Ik6x_B8-5xfT", "mariobarber");
 ?>
